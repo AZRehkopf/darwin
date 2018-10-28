345e1a6282de722efe4e8402f21028492be12515
Andrew Rehkopf <8813337+AZRehkopf@users.noreply.github.com>
2018-10-28 19:39:12 -0400

    added chary.js

---
+++ darwin_interface/visualizations/static/visualizations/charts.js
@@ -0,0 +1,78 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var ctx = document.getElementById("chart2").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: data.labels,
+	        datasets: [{
+	            label: 'generations',
+	            data: data.gen_data,
+	            backgroundColor: [
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+
+	var ctx = document.getElementById("chart1").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: data.labels,
+	        datasets: [{
+	            label: 'execution time',
+	            data: data.exec_data,
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+});
+
+
--- darwin_interface/visualizations/views.py
+++ darwin_interface/visualizations/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Graph
+from .models import Graph, DataPoint
+
+import json
 
 def index(request):
 	graph_list = Graph.objects.order_by('pub_date')
+	
+	labels = []
+	exec_data = []
+	gen_data = []
+	for graph in graph_list:
+		for point in graph.datapoint_set.all():
+			labels.append(point.population_size)
+			exec_data.append(point.avg_execution_time)
+			gen_data.append(point.avg_generations)
+
+	data = 	{
+		'labels': labels,
+		'exec_data': exec_data,
+		'gen_data': gen_data,
+	}
+
+	data = json.dumps(data)
+
 	template = loader.get_template('visualizations/index.html')
 	context = {
 			'graph_list': graph_list,
+			'data': data,
 	}
 	return HttpResponse(template.render(context, request))	
